bbde6054dca58157e81a7f25fb30ee554dacc892
Wandihh <wanderson_ricardo@estudante.sesisenai.org.br>
2025-10-30 16:35:20 -0300

    Inicio das telas cadastro, login e home

---
+++ public/home.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div>
+        <h2>DashCore</h2>
+
+        <a href="http://localhost:8080/vitoria_wanderson/public/logar.php">Sair</a>
+
+    </div>
+
+
+    <div>
+
+        <button>Criar gráfico</button>
+
+        <div>
+
+
+        </div>
+
+
+        <button>Criar dashboard</button>
+
+
+    </div>
+
+    
+</body>
+</html>
+
