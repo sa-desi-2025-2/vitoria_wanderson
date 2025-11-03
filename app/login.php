8ac4eeb11dff5927580e84a2ac619d14807b832b
Wandihh <wanderson_ricardo@estudante.sesisenai.org.br>
2025-11-03 16:21:15 -0300

    /app/login e /app/cadastro finalizados, /app/gateaway iniciado

---
+++ app/login.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once __DIR__ ."conexao.php";
+
+class Usuario{
+
+    private Usuario $usuario;
+    public function __construct() {}
+    private $login;
+    private $senha;
+
+    public function getSenha (){
+
+        return $thiss->senha;
+    }
+
+    public function setSenha($senha){
+
+        $this->senha = $senha;
+    }
+
+    public function getLogin(){
+
+        return $this->login;
+
+    }
+
+    public function setLogin($login){
+
+        $this->login;
+    }
+
+    public function logar(){
+
+        $this->login = $_POST["login"];
+        $this->senha = $_POST["senha"];
+        $this->usuario = new Usuario();
+        $resultado = $this->usuario->buscarPorEmail($this->email;);
+
+        if($resultado->rowCount() == 1){
+
+            $resultado = $resultado->fetch();
+
+            if($resultado['senha'] === hash('sha512', $this->senha;)){
+
+                session_start();
+                $_SESSION['id_usuario'] = $resultado['id_usuario'];
+                $_SESSION['admin'] = $resultado['admin'];
+                $_SESSION['loggedin'] = true;
+                return 1;
+            
+            }else{
+
+                return 0;
+            
+            }else{
+
+                return 0;
+            }
+        }
+
+        public function deslogar(){
+            session_destroy();
+            header("location:.../index.html");
+        }
+
+    }
+
+
+}
+
+
+?>
